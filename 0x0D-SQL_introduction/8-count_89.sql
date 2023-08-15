bc9792d5a0ae9fa71ae189d44bf61886494d356f
saraali <saraali.no3man@gmail.com>
2023-08-15 12:05:45 -0700

    8. Count 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
--- Inserts a new row into the table first_table in my MySQL server.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- Displays the number of records with id = 89 in the table first_table in my MySQL server.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
